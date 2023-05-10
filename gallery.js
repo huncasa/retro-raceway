23644ff0f9056a8fe3d12776da0fb56ef36944e8
huncasa <133078552+huncasa@users.noreply.github.com>
2023-05-10 07:27:11 +0200

    Add files via upload

--- gallery.js
+++ gallery.js
@@ -1,46 +1,61 @@
-var base_path = "/images/";
-var image_list = [];
-
-function loadImages(path) {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-      var images = JSON.parse(xhr.responseText);
-      for (var i = 0; i < images.length; i++) {
-        image_list.push(base_path + path + "/" + images[i]);
-      }
-      if (image_list.length === 450) {
-        createGallery(image_list);
-      }
-    }
-  };
-  xhr.open("GET", path + "/images.json", true);
+function getDirectories(path) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", path, false);
   xhr.send();
+  let result = [];
+  let parser = new DOMParser();
+  let doc = parser.parseFromString(xhr.responseText, "text/html");
+  let dirs = doc.querySelectorAll("a[href*='//']");
+  for (let i = 0; i < dirs.length; i++) {
+    let dirName = dirs[i].href.replace(/.*\//g, "");
+    if (dirName != "..") {
+      result.push(dirName);
+    }
+  }
+  return result;
 }
 
-function createGallery(images) {
-  const galleryContainer = document.createElement("div");
-  galleryContainer.classList.add("gallery-container");
-
-  images.forEach((image) => {
-    const galleryItem = document.createElement("a");
-    galleryItem.href = image.src;
-    galleryItem.setAttribute("data-lightbox", "gallery");
-    const imageElement = document.createElement("img");
-    imageElement.src = image.src;
-    imageElement.alt = image.alt;
-    galleryItem.appendChild(imageElement);
-    galleryContainer.appendChild(galleryItem);
-  });
-
-  const body = document.querySelector("body");
-  body.appendChild(galleryContainer);
-
-  const lightbox = new Lightbox();
-  lightbox.load();
+function getFiles(path) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", path, false);
+  xhr.send();
+  let result = [];
+  let parser = new DOMParser();
+  let doc = parser.parseFromString(xhr.responseText, "text/html");
+  let files = doc.querySelectorAll("a[href*='.jpg'],a[href*='.jpeg'],a[href*='.png'],a[href*='.gif']");
+  for (let i = 0; i < files.length; i++) {
+    let fileName = files[i].href.replace(/.*\//g, "");
+    result.push({
+      name: fileName,
+      path: path + fileName,
+      alt: fileName.replace(/\.[^/.]+$/, "")
+    });
+  }
+  return result;
 }
 
-loadImages("folder1");
-loadImages("folder2");
-loadImages("folder3");
+function loadGallery() {
+  let galleries = document.getElementsByClassName("gallery");
+  for (let i = 0; i < galleries.length; i++) {
+    let gallery = galleries[i];
+    let galleryPath = gallery.getAttribute("data-path");
+    let directories = getDirectories(galleryPath);
+    let files = [];
+    for (let j = 0; j < directories.length; j++) {
+      let folderPath = galleryPath + directories[j] + "/";
+      files = files.concat(getFiles(folderPath));
+    }
+    gallery.innerHTML = generateGalleryHTML(files);
+  }
+}
 
+function generateGalleryHTML(files) {
+  let html = "";
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i];
+    html += "<div class='gallery-item'><img src='" + file.path + "' alt='" + file.alt + "'></div>";
+  }
+  return html;
+}
+
+document.addEventListener("DOMContentLoaded", loadGallery);
